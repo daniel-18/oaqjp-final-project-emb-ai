c0aef1ea907069f33e0e84cfeb6a411c808c582f
daniel-18 <kdaniel12@outlook.fr>
2024-10-27 17:08:21 -0400

    =first commint

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,41 @@
+import requests
+import json
+import numpy as np
+
+def dominant_emotion_finder(emotions):
+    
+    # Find the emotion with the maximum score
+    dominant = max(emotions, key=emotions.get)
+    
+    return dominant
+
+def emotion_detector(text_to_analyse):
+    # Define the URL for the sentiment analysis API
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    # Create the payload with the text to be analyzed
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Set the headers with the required model ID for the API
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(url, json=myobj, headers=header)
+    #format the response using json.loads method
+    formatted = json.loads(response.text)
+    #get the required emotion from the formated response
+    emotions = formatted['emotionPredictions'][0]['emotion']
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+    dominant_emotion = dominant_emotion_finder(emotions)
+    formatted_response = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    return formatted_response
